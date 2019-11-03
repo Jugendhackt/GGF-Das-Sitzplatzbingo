9d4f8a42973a51339c92ad2546b280cae0440403
Torben Wetter <torben@wetter.codes>
2019-11-03 11:09:57 +0100

    Moved javascript to own file

--- frontend/javascript.js
+++ frontend/javascript.js
@@ -1,22 +1,58 @@
-var counter = 0;
+function showTrainInfo(result) {
+    let counter = 0, reihen = 12;
+    result.utilization.forEach(function (element) {
+        let k = parseInt(element.wagonPosition);
 
-var reihen = 12;
-var waegen = 10;
+        let gesamt = element.capacitySeatsFirst + element.capacitySeatsSecond;
+        let reserviert = element.seatReservationsFirst + element.seatReservationsSecond;
+        let auslastung = Math.round(((reserviert / gesamt) * 100));
 
-for(var k = 1; k <= waegen; k++){
-	var hinzu2 = "<div class='wagen' id='wagen" + k + "'></div>";
-	document.getElementById("ice").innerHTML += hinzu2;
-	for(var j = 1; j <= reihen; j++){
-		var hinzu1 = "<div id='" + k + "reihe" + j + "'></div>";
-		document.getElementById("wagen" + k).innerHTML += hinzu1;
-		for(var i = 0; i <= 4; i++){
-			if(i == 2){
-				hinzu = "<div class='zwischenraum'></div>";
-			}else{
-				counter += 1;
-				var hinzu = "<div id='sitz" + counter + "' class='sitz'>" + ((counter - 1) % (4 * reihen) + 1) + "</div>";
-			}
-			document.getElementById(k + "reihe" + j).innerHTML += hinzu;
-		}
-	}
+        let hinzu2 = "<div class='wagen' id='wagen" + k + "'> Auslastung: " + auslastung + "%</div>";
+        document.getElementById("ice").innerHTML += hinzu2;
+        for (let j = 1; j <= reihen; j++) {
+            let hinzu1 = "<div id='" + k + "reihe" + j + "'></div>";
+            document.getElementById("wagen" + k).innerHTML += hinzu1;
+            for (let i = 0; i <= 4; i++) {
+                let hinzu = '';
+                if (i === 2) {
+                    hinzu = "<div class='zwischenraum'></div>";
+                } else {
+                    counter += 1;
+                    if (Math.random() * 100 < auslastung) {
+                        hinzu = "<div id='sitz" + counter + "' class='sitz' style='background: #333333'>" + ((counter - 1) % (4 * reihen) + 1) + "</div>";
+                    } else {
+                        hinzu = "<div id='sitz" + counter + "' class='sitz'>" + ((counter - 1) % (4 * reihen) + 1) + "</div>";
+                    }
+
+                }
+                document.getElementById(k + "reihe" + j).innerHTML += hinzu;
+            }
+        }
+    });
 }
+
+$("button#search").on("click", function (event) {
+    event.preventDefault();
+
+    const startort = encodeURIComponent($('input#startort').val()),
+        zugnummer = $('input#zugnummer').val(),
+        datum = $('input#datum').val();
+
+    $.ajax({
+        url: "https://ggf-backend.margau.me/seats/" + zugnummer + "/" + datum + "/" + startort,
+        type: "GET",
+        crossDomain: true,
+        success: function (result) {
+            showTrainInfo(result);
+        },
+        error: function (result) {
+            let error;
+            if ('responseText' in result) {
+                error = JSON.parse(result.responseText).error;
+            } else {
+                error = 'Unbekannter Fehler'
+            }
+            alert('Fehler: ' + error);
+        }
+    });
+});
